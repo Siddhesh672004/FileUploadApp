044a0e6b0fa39bf530d11a77b75db5f1741cdde4
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-09-28 13:36:27 +0530

    Schema Defined & server config

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+//load config from .env file
+const PORT = process.env.PORT || 3000;
+
+//Middleware to parse json request body
+app.use(express.json());
+const fileupload = require("express-fileupload");
+app.use(fileupload());
+
+
+//connect to the database
+require("./config/database").connect();
+
+//cloudinary connection
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+
+//Api routes mounting
+const Upload = require("./routes/FileUpload");
+app.use('api/v1/upload', Upload);
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server started successfully at ${PORT}`);
+})
+
+//Default route
+app.get("/", (req, res) => {
+    res.send(`<h1> This is HOMEPAGE </h1>`);
+} )
---
+++ models/File.js
@@ -0,0 +1,23 @@
+//import mongoose
+const mongoose = require("mongoose");
+
+//route handler
+const fileSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true,
+        },
+        imageUrl:{
+            type:String,
+        },
+        tags:{
+            type:String,
+        },
+        email:{
+            type:String,
+        }
+    });
+
+//export
+module.exports = mongoose.model("user", fileSchema);
