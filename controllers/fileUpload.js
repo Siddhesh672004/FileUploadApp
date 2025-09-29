f58630ec7592c974774ecc7ddf168d4618cf1294
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-09-29 13:42:38 +0530

    imageSizeReducer controller implemented

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -35,8 +35,13 @@ function isFileTypeSupported(type, supportedTypes){
 }
 
 //Function to uploade image on cloudinary
-async function uploadFileToCloudinary(file, folder){
+async function uploadFileToCloudinary(file, folder, quality){
     const options = {folder};
+
+    if(quality){
+        options.quality = quality;
+    }
+    
     options.resource_type = "auto";
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
@@ -127,6 +132,55 @@ exports.videoUpload = async (req, res) => {
             success:true,
             imageurl: response.secure_url,
             message:"Video uploaded successfully",
+        })       
+
+    }
+    catch(error){
+        console.error(error);
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong",
+        })
+    }
+}
+
+//imageSizeReducer
+exports.imageSizeReducer = async (req, res) => {
+    try{    
+        //data fetch
+        const { name, tags, email } = req.body;
+        // console.log(name, tags, email);
+
+        const file = req.files.imageFile;
+        // console.log(file);
+
+        //validationfor file supported or not
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+
+        if(!isFileTypeSupported(fileType, supportedTypes)){
+            return res.status(400).json({
+                success: false,
+                message: "File format not supported",
+            })
+        }
+
+        // agar file format supported hai to...
+        const response = await uploadFileToCloudinary(file, "FileUploadApp", 30);
+        console.log(response);
+
+        //db me entry save krni hai
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl: response.secure_url,
+        })
+
+        res.json({
+            success:true,
+            imageurl: response.secure_url,
+            message:"Image uploaded successfully",
         })       
 
     }
